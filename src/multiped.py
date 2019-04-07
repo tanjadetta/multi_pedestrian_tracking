0727e2443f8d5721f3ced524026114a08affae0c
tanjadetta <tanja.detta@gmx.de>
2019-04-07 14:56:26 +0200

    #2: Add a contructor, some members and a main

--- src/multiped.py
+++ src/multiped.py
@@ -1,1 +1,22 @@
-//the tracker
+#the tracker
+
+class multiped:
+	framePath  = ""
+	startFrame = 0
+	endFrame   = 0
+	
+	def __init__(self, fPath, startF, endF):
+		self.framePath  = fPath
+		self.startFrame = startF
+		self.endFrame   = endF
+	
+	def info(self):
+		print("The Trackers looks for Frames in Path " + self.framePath)
+		print("With Startframe: " + str(self.startFrame) + " and Endframe: " + str(self.endFrame))
+		
+if __name__ == '__main__':
+	thetracker = multiped("c:/herearetheframes/", 1, 1000)
+	thetracker.info()
+	
+	
+	
