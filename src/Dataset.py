f41d897cc039caa921caff1e29caccb1b35cab66
tanjadetta <tanja.detta@gmx.de>
2019-04-08 01:16:50 +0200

    #2, #3 implemnts getNextFrame in Dataset

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,19 +1,44 @@
+import glob
+import numpy as np
+import cv2
+
 class Dataset:
 	path = ""
 	startFrame = 0
 	endFrame   = 0
+	aktuFrameIndex = 0
+	frameList = None
 	
-	def __init__(self, path, startF, endF):
+	def __init__(self, path, fileExt):
 		self.path = path
-		self.startFrame = startF
-		self.endFrame = endF
-
+		self.aktuFrameIndex = -1
+		self.fileExt = fileExt
+		self.frameList = glob.glob(path + "/*." + fileExt)
+		self.aktuFrame = ""
+		self.frameCount = 0
+		self.im = None
+		self.frameCount = len(self.frameList)
+		
 	def info(self):
 		return "Dataset with \n" \
 		"path = "       + str(self.path) + "\n"  \
-		"startFrame = " + str(self.startFrame) + "\n" \
-		"endFrame = " + str(self.endFrame)
+		"file Ext: = " + str(self.fileExt) + "\n" \
+		"Count " + str(self.frameCount) + " Frames."
+		
+	def getNextFrame(self):
+		if self.aktuFrameIndex < self.frameCount - 1:
+			self.aktuFrameIndex = self.aktuFrameIndex + 1
+			self.aktuFrame = self.frameList[self.aktuFrameIndex]
+			self.im = cv2.imread(self.aktuFrame)
+			return True
+		else:
+			return False
 
 if __name__ == '__main__':
-	ds = Dataset("c:/herearetheframes/", 1, 1000)
+	ds = Dataset("c:/here_are_the_frames/", "jpg")
 	print (ds.info())
+	while ds.getNextFrame():
+		print("Frame: ", ds.aktuFrameIndex, " at ", ds.aktuFrame)
+		cv2.imshow("Hallo", ds.im)
+		cv2.waitKey()
+		
--- src/Multiped.py
+++ src/Multiped.py
@@ -1,4 +1,5 @@
 #the tracker
+import cv2
 
 import Dataset
 
@@ -10,10 +11,10 @@ class Multiped:
 		self.dataset = ds
 	
 	def info(self):
-		return "The Trackers uses the following Dataset: " + self.dataset.info()
+		return "The Tracker uses the following Dataset: " + self.dataset.info()
 		
 if __name__ == '__main__':
-	ds = Dataset.Dataset("c:/here_are_the_frames/", 1, 1000)
+	ds = Dataset.Dataset("c:/here_are_the_frames/", "jpg")
 	thetracker = Multiped(ds)
 	print (thetracker.info())
 	
