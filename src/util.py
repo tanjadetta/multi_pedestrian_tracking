7db457258cef8b59890a72dad011ee3ce8ed1647
tanjadetta <tanja.detta@gmx.de>
2019-04-11 16:18:08 +0200

    #6

--- src/util.py
+++ src/util.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 
-
 def BGRtoLAB(im):
 	lab_image = cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
 	return lab_image
@@ -57,13 +56,71 @@ def movePicture(im, directionX, directionY):
 
 def clamp(x, a, b):
 	return min(max(x, a), b)
+
+
+def almostEqual(a,b):
+	return np.abs(a - b) < 0.00000001
+
+def magAndAngle(im):
+	#img = np.float32(im) / 255.0
+	img = np.float32(im) / 255.0
+	# Calculate gradient 
+	gx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=1)
+	gy = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=1)
+	#cv2.imshow("Sobel x:", gx)
+	#cv2.imshow("Sobel y:", gy)
+	mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+	angle = angle % 180
+	cv2.imshow("MAG:", mag)
+	cv2.imshow("Angle:", angle)
+	cv2.waitKey()
+	print("max,min in angle:", np.max(angle), np.min(angle))
+	print("max,min in mag:", np.max(mag), np.min(mag))
+	#for each pixel chose the channel with the biggest magmitude:
+	h, w, c = np.shape(im)
+	mag2 = mag.reshape(h*w, c).T
+	ang2 = angle.reshape(h*w, c).T
+	M = np.max(mag2, axis=0)
+	G = [None] * c
+	for i in range(c):
+		G[i] = almostEqual(mag2[i], M)			#compare ith row
+		ang2[i] = ang2[i] * G[i]
+	
+	ang2 = np.max(ang2, axis=0)
+	ang2 = ang2.reshape(h, w)
+	M = M.reshape(h, w)
+	
+	return M, ang2
+
+def plotHelper(im, titel, warte=True):
+	ma = np.max(im)
+	mi = np.min(im)
+	r = ma - mi
+	if almostEqual(r, 0):
+		im2 = im - mi
+	else:
+		im2 = (255.0/r)*(im - mi)
+	im2 = im2.astype(np.uint8)
+	cv2.imshow(titel, im2)
+	if warte == True:
+		cv2.waitKey()
+	else:
+		cv2.waitKey(10)
 	
 if __name__ == '__main__':
-	#test movePicture
+	#test magAndAnngle
+	im = cv2.imread("C:/here_are_the_frames/00000002.jpg")
+	mag, angle = magAndAngle(im)
+	plotHelper(mag, "MAG", False)
+	plotHelper(angle, "angle", False)
+	cv2.waitKey()
+	print(mag)
+	exit()
+	#test clamp
 	print(clamp(14, 10,20))
 	print(clamp(4, 10,20))
 	print(clamp(24, 10, 20))
-	
+	#test movePicture
 	im = cv2.imread("C:/here_are_the_frames/test/001.jpg")
 	moves = [ [0,0], [30,0], [0,30], [-30,0], [0,-30], [30,30], [-30,-30], [30,-30], [-30,30] ]
 	for t in moves:
